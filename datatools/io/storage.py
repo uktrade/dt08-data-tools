fd0607916ac7ec717e3b7cd5422e23dd683774c3
Cammil Taank <ctaank@gmail.com>
2017-06-21 13:28:16 -0400

    Add Local Storage wrapper

--- datatools/io/storage.py
+++ datatools/io/storage.py
@@ -44,8 +44,8 @@ class S3Storage(Storage):
         b.delete_objects(Delete={
             'Objects': [
                 {'Key': file_name}
-                ]
-            }
+            ]
+        }
         )
 
     def write_file(self, file_name, data):
@@ -54,7 +54,7 @@ class S3Storage(Storage):
 
     def read_file(self, file_name):
         obj = self._get_s3_object(file_name)
-        return obj.get()['Body'].read()
+        return obj.get()['Body'].read().decode()
 
     def get_file_names(self):
         bucket = self._get_bucket()
@@ -63,3 +63,33 @@ class S3Storage(Storage):
 
     def create_storage(self, folder_name):
         raise NotImplemented()
+
+
+class LocalStorage(Storage):
+
+    def __init__(self, base_path):
+        self.base_path = base_path
+        self._path = Path(base_path)
+
+    def get_file_names(self):
+        yield from self._path.glob('**/*')
+
+    def write_file(self, file_name, data):
+        path = self._full_path(file_name)
+        with open(path, 'w') as file:
+            file.write(data)
+
+    def read_file(self, file_name):
+        path = self._full_path(file_name)
+        with open(path) as file:
+            return file.read()
+
+    def delete_file(self, file_name):
+        path = self._full_path(file_name)
+        os.remove(path)
+
+    def create_storage(self, folder_name):
+        raise NotImplemented()
+
+    def _full_path(self, file_name):
+        return os.path.join(self.base_path, file_name)
