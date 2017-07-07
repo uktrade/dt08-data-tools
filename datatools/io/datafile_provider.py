6bbc7c84284deb99de3c4b8e477b5b28b49d907a
Cammil Taank <ctaank@gmail.com>
2017-07-07 15:10:28 +0100

    read all datafiles in storage

--- datatools/io/datafile_provider.py
+++ datatools/io/datafile_provider.py
@@ -1,5 +1,9 @@
+from collections import namedtuple
 from io import BytesIO
 import zipfile
+
+
+FileInfo = namedtuple('FileInfo', 'name data')
 
 
 class DatafileProvider:
@@ -23,7 +27,7 @@ class DatafileProvider:
         """
         Read the single file or unpack the zip and read those files.
 
-        Returns pairs of file names and their binary data
+        Returns FileInfo objects (file name, and binary data)
         """
         if file_name.endswith('.zip'):
             data = self.storage.read_file(file_name, binary=True)
@@ -32,6 +36,11 @@ class DatafileProvider:
             for m in members:
                 if m in self.ignore_filename_patterns:
                     continue
-                yield m, zf.open(m)
+                yield FileInfo(m, zf.open(m))
         else:
-            yield file_name, self.storage.read_file(file_name, binary=True)
+            yield FileInfo(file_name, self.storage.read_file(file_name, binary=True))
+
+    def read_files_all(self):
+        for fn in self.get_file_names():
+            yield from self.read_files(fn)
+
