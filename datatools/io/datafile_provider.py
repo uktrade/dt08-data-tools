5a0fd4aa73a7a668c048244c13df18cdb6156c03
Cammil Taank <ctaank@gmail.com>
2017-07-08 09:48:46 +0100

    ability to choose whether to unpack datafile or read as single file

--- datatools/io/datafile_provider.py
+++ datatools/io/datafile_provider.py
@@ -11,6 +11,39 @@ class DatafileProvider:
 
     def get_file_names(self):
         for fn in self.storage.get_file_names():
-            if fn in self.ignore_filename_patterns:
+            if self._should_ignore(fn):
                 continue
             yield fn
+
+    def read_files(self, file_name, unpack=False):
+        """
+        Read the single file or unpack the zip and read those files.
+
+        Returns FileInfo objects (file name, and binary data)
+        """
+        if file_name.endswith('.zip') and unpack:
+            bdata = self.storage.read_file(file_name, binary=True)
+            yield from self.read_files_from_zip(bdata)
+        else:
+            yield FileInfo(file_name, self.storage.read_file(file_name, binary=True))
+
+    @classmethod
+    def _should_ignore(cls, filename):
+        if filename in cls.ignore_filename_patterns:
+            return True
+        if filename.startswith('__MACOSX'):
+            return True
+        return False
+
+    def read_files_all(self, unpack=False):
+        for fn in self.get_file_names():
+            yield from self.read_files(fn, unpack=unpack)
+
+    @classmethod
+    def read_files_from_zip(cls, bdata):
+        zf = zipfile.ZipFile(BytesIO(bdata), mode='r')
+        members = zf.namelist()
+        for m in members:
+            if cls._should_ignore(m):
+                continue
+            yield FileInfo(m, zf.open(m))
--- tests/io/test_datafile_provider.py
+++ tests/io/test_datafile_provider.py
@@ -1,4 +1,12 @@
-from datatools.io.storage import Storage
+import os
+import tempfile
+import uuid
+import zipfile
+
+import pytest
+from contextlib import contextmanager
+
+from datatools.io.storage import Storage, LocalStorage
 from datatools.io.datafile_provider import DatafileProvider
 
 
@@ -19,7 +27,31 @@ class StorageMock(Storage):
         raise NotImplementedError
 
 
-def test_datafile_provider():
-    storage = StorageMock()
-    dfp = DatafileProvider(storage)
-    assert list(dfp.get_file_names()) == ['file1', 'file2']
+class TestDatafileProvider:
+
+    def test_get_file_name(self):
+        storage = StorageMock()
+        dfp = DatafileProvider(storage)
+        assert list(dfp.get_file_names()) == ['file1', 'file2']
+
+    def test_read_files(self):
+        with tmp_zipfile() as tmpdirname:
+            # run test
+            storage = LocalStorage(tmpdirname)
+            dfp = DatafileProvider(storage)
+            for name, data in dfp.read_files('test-zipfile.zip', unpack=True):
+                assert name == 'some-file.txt'
+                assert data.read().decode() == 'test-data'
+
+    def test_read_files_from_zip(self):
+        pass
+
+@contextmanager
+def tmp_zipfile():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # create zip file to test extraction
+        zf_path = os.path.join(tmpdirname, 'test-zipfile.zip')
+        zf = zipfile.ZipFile(zf_path, mode='w')
+        zf.writestr('some-file.txt', data='test-data')
+        zf.close()
+        yield tmpdirname
